7072e450fa4036a3c58a24e1d9f8f238a7fbc161
ivanklymenko <klymenia@fuib.com>
2025-04-22 15:05:57 +0200

    spot_3x_10x_20x.tflite added

--- app/src/main/java/tech/ai_robotics/drone_shooter_2/object_detection/Constants.kt
+++ app/src/main/java/tech/ai_robotics/drone_shooter_2/object_detection/Constants.kt
@@ -1,9 +1,10 @@
 package tech.ai_robotics.drone_shooter_2.object_detection
 
 object Constants {
-    const val MODEL_PATH = "yolo8_3x_180325.tflite"
+    const val MODEL_PATH = "spot_3x_10x_20x.tflite"
     const val LABELS_PATH = "labels.txt"
 }
 
-// TODO: yolo8_3x_180325.tflite under 50 m 3x zoom Samsung S21 Ultra. Data set Object Detection 2
-// TODO: yolo8_1x_180325.tflite under 50 m 1x zoom Samsung S21 Ultra. Data set Object Detection 1
+// TODO: yolo8_3x_180325.tflite under 50 m 3x zoom Samsung S21 Ultra. object. Data set Object Detection 2
+// TODO: yolo8_1x_180325.tflite under 50 m 1x zoom Samsung S21 Ultra. object. Data set Object Detection 1
+// TODO: spot_3x_10x_20x.tflite spot the balcony opposite Samsung S21 Ultra. objects. Data set Object Detection 3
