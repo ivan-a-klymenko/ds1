0eddb9f0bd060352f2c9decd1d94e967f3fefe8d
ivanklymenko <ivan.a.kllymenko@gmail.com>
2026-03-10 09:50:26 +0100

    SERVER_HOST fixed

--- app/src/main/java/tech/ai_robotics/drone_shooter_2/net/ClientConfig.kt
+++ app/src/main/java/tech/ai_robotics/drone_shooter_2/net/ClientConfig.kt
@@ -5,7 +5,7 @@ object ClientConfig {
     const val CLIENT_ID = "my-client-001"
 
     // URL сервера; по умолчанию локалхост - используется adb/usb forward или USB Ethernet
-    const val SERVER_HOST = "http://127.0.0.1:8080"
+    const val SERVER_HOST = "http://192.168.0.101:8080"
     const val REPORT_PATH = "/report"
 }
 
